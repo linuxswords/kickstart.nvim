64ef80704b790959e353b77438236f60d92db98a
Martin Knoller Stocker <martin.knoller@brackalltron.ch>
2025-04-14 16:54:30 +0200

    chore(plugin) enable pair module

--- init.lua
+++ init.lua
@@ -967,6 +967,9 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      -- pair module
+      require('mini.pairs').setup { ignore_blank_line = true }
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
