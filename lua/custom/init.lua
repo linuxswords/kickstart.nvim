f47a0ec2189140dd34229099f46f449f248ad49d
Martin Knoller Stocker <martin.knoller@brackalltron.ch>
2025-05-22 08:10:05 +0200

    fix

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -6,9 +6,8 @@ vim.opt.relativenumber = true
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 -- martin rather has case sensitive search
 vim.opt.ignorecase = false
-vim
-  -- keymap for writing buffer
-  .vim
-  .keymap
-  .set('n', '<F4>', ':w<Cr>')
+vim.opt.smartcase = true
+
+-- keymap for writing buffer
+vim.keymap.set('n', '<F4>', ':w<Cr>')
 vim.keymap.set('i', '<F4>', '<Esc><F4><F4>')
