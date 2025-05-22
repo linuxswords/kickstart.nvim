9a3cf9cddfaa3a3d19906a27d3fdea933072a408
Martin Knoller Stocker <martin.knoller@brackalltron.ch>
2025-05-22 07:17:04 +0200

    fzf: <C-p> searches gitfiles

--- init.lua
+++ init.lua
@@ -435,6 +435,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
